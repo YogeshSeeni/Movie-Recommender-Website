dcd53cec8e3cb275b28c9c8536e883c20e238a3c
YogeshSeeni <54773171+YogeshSeeni@users.noreply.github.com>
2020-05-29 11:44:54 -0400

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-config = {
-    "apiKey": "AIzaSyB-V0NyRynPW1MXxtmQ5H1kVjGaHZERGLg",
-    "authDomain": "movie-recommendor-c013f.firebaseapp.com",
-    "databaseURL": "https://movie-recommendor-c013f.firebaseio.com",
-    "projectId": "movie-recommendor-c013f",
-    "storageBucket": "movie-recommendor-c013f.appspot.com",
-    "messagingSenderId": "1016909747943",
-    "appId": "1:1016909747943:web:dccf023acb0e09908a3442"
-}
